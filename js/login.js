b8d469aaf30a1275049769eea0196090ebdb1910
Yomira Martinez <yomiradmartinez@gmail.com>
2020-09-01 21:41:11 -0300

    Validations  are made

--- js/login.js
+++ js/login.js
@@ -1,6 +1,29 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+var user = document.getElementById('username')
+var email = document.getElementById('email')
+var password = document.getElementById('password')
+
 document.addEventListener("DOMContentLoaded", function(e){
 
+document.getElementById('button').addEventListener("click", function(e){
+    valid = true;
+
+        if (user.value == ''){
+            alert("Ingrese su usuario");
+            valid = false;
+        }
+
+        if (email.value == ''){
+            alert("Ingrese su e-mail");
+            valid = false;
+        }
+
+        if (password.value == ''){
+            alert("Ingrese su contraseña");
+            valid = false;
+        }
+    });
 });
